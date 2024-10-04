aafea17256e726f458e6cfc24b51f0b8d02f20f7
Erik Michiels <e.michiels1988-github@yahoo.com>
2024-10-04 15:35:22 +0200

    Added class Information

--- main.py
+++ main.py
@@ -7,22 +7,20 @@ api = FastAPI()
 charger = minimalmodbus.Instrument('/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', 50)
 charger.serial.baudrate = 57600
 charger.serial.stopbits = 2
+
+class Information(BaseModel):
+    firmware_version: str
+    serial_number: str
+    hardware_version: int
+    product_id: int
     
 
-@api.get("/firmware/version")
-def read_firmware_version():
-    return {"firmware_version": charger.read_string(0x0001)}
+@api.get("/information")
+def read_information():
+    return Information(
+        firmware_version=charger.read_string(0x0001),
+        serial_number=charger.read_string(0x0009),
+        hardware_version=charger.read_register(0x0011),
+        product_id=charger.read_register(0x0012)
+    )
 
-@api.get("/status")
-def read_status():
-    status_map = {
-        0: "Not initialized",
-        1: "Idle",
-        2: "Connected",
-        3: "Pre-condition valid",
-        4: "Ready to charge",
-        5: "Charging",
-        6: "Error",
-        7: "Service mode"
-    }
-    return {"status": status_map[charger.read_register(0x0000)]}
