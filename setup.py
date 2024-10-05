7bf0efa425c93c01c86108a3645e09507edc68d7
Erik Michiels <e.michiels1988-github@yahoo.com>
2024-10-05 15:26:57 +0200

    Fix typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'mennekes-smart-charge=mennekes_smart_charge_api.main:main'
+            'mennekes-smart-charge=mennekes_smart_charge.main:main'
         ]
     },
     install_requires=[
